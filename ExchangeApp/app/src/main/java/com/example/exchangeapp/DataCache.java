0c56cdfee10d19097e6bc0e92f6c237256fb8bb7
Rachel Fullerton <rachfull00@gmail.com>
2022-05-27 13:34:04 +0200

    Home screen fragment formatting close to finish. Need expandablelistview

---
+++ ExchangeApp/app/src/main/java/com/example/exchangeapp/DataCache.java
@@ -0,0 +1,4 @@
+package com.example.exchangeapp;
+
+public class DataCache {
+}
--- ExchangeApp/app/src/main/java/com/example/exchangeapp/MainActivity.kt
+++ ExchangeApp/app/src/main/java/com/example/exchangeapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.exchangeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var currFrag = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currFrag == null) {
+            currFrag = WelcomeFragment()
+            //  currFrag.registerListener(this)
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, currFrag)
+                .commit()
+        }
     }
 }
